02ca3d182a2f11a970d0d6e9f9c38d96a1ab67a4
Marcelo Carte <marcelo.carte.munoz@gmail.com>
2024-11-24 02:28:48 -0300

    commiting

---
+++ README.md
@@ -0,0 +1,3 @@
+M4-Desafio 2
+
+Mcarte.-
---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+// import Home from "./pages/Home";
+import Register from "./pages/Register";
+// import Login from "./pages/Login";
+
+function App() {
+  return (
+    <>
+      {/*      <Home />
+       */}
+      <Register />
+      {/* <Login /> */}
+    </>
+  );
+}
+
+export default App;
